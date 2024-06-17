64ef861ee2717ff8977760a9e607240b51e27138
christoferNK22 <christofernathanael116@gmail.com>
2024-06-17 19:01:56 +0700

    fix: mongoose.connect

--- app.js
+++ app.js
@@ -16,37 +16,14 @@ app.post('/articles', (req, res) => {
     res.send(req.body);
 });
 
-const uri = "mongodb+srv://christofernathanael116:8uBzTgIqxssAtGa4@dialog-sunyi.qlyn7xg.mongodb.net/?retryWrites=true&w=majority&appName=Dialog-Sunyi";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
 
 app.listen(3000, () => {
     console.log('server is running');
 });
 
 
-// Connect to MongoDB
-mongoose.connect(uri, { /* connection options */ })
+mongoose.connect("mongodb+srv://christofernathanael116:8uBzTgIqxssAtGa4@dialog-sunyi.qlyn7xg.mongodb.net/Dialog-Sunyi?retryWrites=true&w=majority&appName=Dialog-Sunyi", 
+    { /* connection options */ })
   .then(() => {
     console.log("Connected to database");
 })
