a5efa7a652254543749f2075c1f485cf703f07db
christoferNK22 <christofernathanael116@gmail.com>
2024-06-18 06:25:36 +0700

    add: read api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const Article = require('./models/article.model.js');
+const Class = require('./models/class.model.js');
 const app = express();
 
 app.use(express.json());
@@ -19,12 +20,50 @@ app.get('/articles/:id', async (req, res) => {
     } catch {
         res.status(500).json({message: error.message});
     }
-})
+});
 
 app.post('/articles', async (req, res) => {
     try {
         const article =  await Article.create(req.body);
         res.status(200).json(article)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+app.get('/classes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const classes = await Class.findById(id);
+        res.status(200).json(classes)
+    } catch {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.post('/classes', async (req, res) => {
+    try {
+        const classes =  await Class.create(req.body);
+        res.status(200).json(classes)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+app.get('/comments/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const comment = await Comment.findById(id);
+        res.status(200).json(comment)
+    } catch {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.post('/comments', async (req, res) => {
+    try {
+        const comment =  await Comment.create(req.body);
+        res.status(200).json(comment)
     } catch (error) {
         res.status(500).json({message: error.message})
     }
