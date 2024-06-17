6794509633f65fb75b43f07a42dae1b6ec9843fc
christoferNK22 <christofernathanael116@gmail.com>
2024-06-18 05:44:05 +0700

    add: class.model.js

---
+++ models/class.model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const ClassSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "please enter title"]
+        },
+        organizer: {
+            type: String,
+            required: true
+        },
+        realizationDate: {
+            type: Date, 
+            required: true
+        },
+        registrationDate: {
+            type: Date,
+            required: true
+        },
+        posterImg: {
+            type: String,
+            required: true
+        },
+        link: {
+            type: String,
+            required: true 
+        },
+        fee: {
+            type: Number,
+            required: false,
+            default: 0
+        }
+    }
+);
+
+const Class = mongoose.model("Class", ClassSchema)
+
+module.exports = Class
