1dc7e8d645b01822416f6729006a1eb487b9f83f
christoferNK22 <christofernathanael116@gmail.com>
2024-06-19 16:05:08 +0700

    feat: error and succes message

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -3,9 +3,16 @@ const Article = require('../models/article.model');
 const getArticles =  async (req, res) => {
     try {
         const articles = await Article.find();
-        res.status(200).json(articles)
-    } catch {
-        res.status(500).json({message: error.message});
+        res.status(200).json({
+            status: "success",
+            message: "Berhasil mendapatkan semua artikel",
+            data: articles
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: "Kesalahan server",
+        });
     }
 }
 
@@ -13,20 +20,56 @@ const getArticle = async (req, res) => {
     try {
         const { id } = req.params;
         const article = await Article.findById(id);
-        res.status(200).json(article)
-    } catch {
-        res.status(500).json({message: error.message});
+        
+        if (!article) {
+            return res.status(404).json({
+              status: "error",
+              message: "Artikel tidak ditemukan",
+            });
+          }
+      
+          res.status(200).json({
+            status: "success",
+            message: "Berhasil mendapatkan artikel",
+            data: article,
+          });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+          status: "error",
+          message: "Kesalahan server",
+        });
     }
 };
 
 const createArticle = async (req, res) => {
     try {
-        const article =  await Article.create(req.body);
-        res.status(200).json(article)
+      const newArticle = new Article(req.body);
+      const savedArticle = await newArticle.save();
+      res.status(201).json({
+        status: "success",
+        message: "Artikel berhasil dibuat",
+        data: savedArticle,
+      });
     } catch (error) {
-        res.status(500).json({message: error.message})
+      console.error(error);
+  
+      if (error.name === 'ValidationError') {
+        const validationErrors = Object.values(error.errors).map(err => err.message);
+        return res.status(400).json({
+          status: "error",
+          message: "Validasi gagal",
+          errors: validationErrors,
+        });
+      }
+  
+      res.status(500).json({
+        status: "error",
+        message: "Kesalahan server",
+      });
     }
-};
+  };
+  
 
 module.exports = {
     getArticles,
